86a4664c456256fe0ee990c1443d12e21bb620f7
xTrM-FR <44271159+xTrM-FR@users.noreply.github.com>
2018-11-08 20:59:59 +0100

    Updated Uninstaller [nKoeur 3.0.0+]

--- files/uninstall/uHelper.bat
+++ files/uninstall/uHelper.bat
@@ -1,18 +1,11 @@
 @echo off
 
-:: On supprime les fichiers installés dans l'ordinateur
+:: On supprime les fichiers installÃ©s dans l'ordinateur
 rd /s /q "%USERPROFILE%/nKoeur"
 
-:: Cette ligne sera enlevée pour la version v2.0.1 BUBBLE de nKoeur,
-:: qui utilisera une techenique plus avancée pour le démarage
-:: du programme au startup de Windows (sysrgstr)
-
+:: Deletes the regedit key and the launcher.bat
+:: in startup for the old nKoeur installer
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run\nKoeur" /f
 del /A /f "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\launcher.bat"
-
-:: Création d'un fichier pendant 3 secondes, permettant
-:: de savoir si nKoeur s'est correctement desinstallé (DEBUG)
-echo true > uninstall.ed
-PING localhost -n 4 >NUL
-del /A /f uninstall.ed
 
 exit
